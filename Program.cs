665ff60974a39a44c0680cc460a9cf091493908c
Islam <islomjons765@gmail.com>
2026-04-28 18:05:10 +0300

    Commit

--- Program.cs
+++ Program.cs
@@ -1,1 +1,45 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Bu arifmetik amallarini bajarish uchun programma");
+Console.WriteLine("Ishbu programmani ishlatish uchun sonlarni va amallarni kiritishingiz kk buladi");
+
+Console.WriteLine("Birinchi son: ");
+decimal userNum1 = Convert.ToDecimal(Console.ReadLine());
+
+Console.WriteLine("Ikkinchi son: ");  
+decimal userNum2 = Convert.ToDecimal(Console.ReadLine());
+
+Console.WriteLine("Operator (+, -, *, /): ");
+string oper = Console.ReadLine(); 
+
+decimal result;
+
+if (oper == "*")  
+{
+    result = userNum1 * userNum2;
+    Console.WriteLine($"Natija: {userNum1} * {userNum2} = {result}");
+}
+else if (oper == "+")
+{
+    result = userNum1 + userNum2;
+    Console.WriteLine($"Natija: {userNum1} + {userNum2} = {result}");
+}
+else if (oper == "-")
+{
+    result = userNum1 - userNum2;
+    Console.WriteLine($"Natija: {userNum1} - {userNum2} = {result}");
+}
+else if (oper == "/")
+{
+    if (userNum2 != 0)
+    {
+        result = userNum1 / userNum2;
+        Console.WriteLine($"Natija: {userNum1} / {userNum2} = {result}");
+    }
+    else
+    {
+        Console.WriteLine("Xato: Nolga bo'lish mumkin emas!");
+    }
+}
+else
+{
+    Console.WriteLine("Xato: Noto'g'ri operator!");
+}
